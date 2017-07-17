6a04050f9f5e9679f22d7d10893b4a413a246106
Valentin Shirokov <regellosigkeitsaxiom@yandex.ru>
2017-07-17 17:16:50 +0300

    Added nix 'installer'

---
+++ default.nix
@@ -0,0 +1,32 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, ansi-terminal, base, bytestring, cryptohash
+      , directory, stdenv
+      }:
+      mkDerivation {
+        pname = "clean-by-hash";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          ansi-terminal base bytestring cryptohash directory
+        ];
+        homepage = "http://github.com/githubuser/clean-by-hash#readme";
+        description = "Simple project template from stack";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
