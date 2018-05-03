6300915fe6154c98a400cc85ca816ac21c94794e
Valentin Shirokov <regellosigkeitsaxiom@yandex.ru>
2018-05-04 04:03:48 +0800

    Added release.nix for hydra

--- release.nix
+++ release.nix
@@ -1,24 +1,9 @@
 let
   pkgs = import <nixpkgs> {};
+  source = import ./default.nix {};
 
   jobs = rec {
-
-    tarball =
-      pkgs.releaseTools.sourceTarball {
-        name = "hello-tarball";
-        src = <hello>;
-        buildInputs = (with pkgs; [ gettext texLive texinfo ]);
-      };
-
-    build =
-      { system ? builtins.currentSystem }:
-
-      let pkgs = import <nixpkgs> { inherit system; }; in
-      pkgs.releaseTools.nixBuild {
-        name = "hello";
-        src = jobs.tarball;
-        configureFlags = [ "--disable-silent-rules" ];
-      };
+    build = source;
   };
 in
   jobs
